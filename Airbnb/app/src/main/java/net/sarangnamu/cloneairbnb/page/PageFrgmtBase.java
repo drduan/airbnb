f53c3b68cb5cc75354f147989d4cc36a53eb8d6e
최철동 <aucd29@choecheoldongui-MacBook-Pro.local>
2016-03-23 13:11:44 +0900

    - added page fragment base file

---
+++ Airbnb/app/src/main/java/net/sarangnamu/cloneairbnb/page/PageFrgmtBase.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2016. Burke Choi All rights reserved.
+ *             http://www.sarangnamu.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.sarangnamu.cloneairbnb.page;
+
+import net.sarangnamu.common.ui.tab.InflateFrgmtBase;
+
+/**
+ * Created by <a href="mailto:aucd29@gmail.com">Burke Choi</a> on 2016. 3. 23.. <p/>
+ */
+public class PageFrgmtBase extends InflateFrgmtBase {
+    private static final org.slf4j.Logger mLog = org.slf4j.LoggerFactory.getLogger(PageFrgmtBase.class);
+
+    @Override
+    protected void initLayout() {
+        mBaseView.setAlpha(0);
+        mBaseView.setTranslationX(dpToPixel(80));
+        mBaseView.animate().alpha(1).setDuration(200).translationX(dpToPixel(0));
+    }
+}
